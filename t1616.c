91a067e6b994ed49994eaa657eb41ba3198daf87
John Honniball <coredump@virginmedia.com>
2019-11-26 22:54:22 +0000

    Complete rearrangement of timer setup. TCA0 now used for three PWM channels (for RGB LED) and TCB0 generates 1ms interrupts. No more blocking in the main loop, all timing done with 'millis()'. RGB LEDs fade up and down using 8-bit PWM.

--- t1616.c
+++ t1616.c
@@ -13,6 +13,7 @@
 #define LED_B PIN2_bm   // Blue LED on PB2
 
 volatile uint32_t Milliseconds = 0UL;
+volatile uint8_t Tick = 0;
 
 ISR(USART0_RXC_vect)
 {
@@ -27,12 +28,13 @@ ISR(USART0_TXC_vect)
 }
 
 
-/* TCA0_OVF_vect --- ISR for Timer/Counter 0 overflow, used for 1ms ticker */
+/* TCB0_OVF_vect --- ISR for Timer/Counter 0 overflow, used for 1ms ticker */
 
-ISR(TCA0_OVF_vect)
+ISR(TCB0_INT_vect)
 {
-   TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
+   TCB0.INTFLAGS = TCB_CAPT_bm;
    Milliseconds++;
+   Tick = 1;
    PORTA.OUTTGL = PIN2_bm;    // DEBUG: 500Hz on PA2 pin
 }
 
@@ -48,46 +50,56 @@ uint32_t millis(void)
    return (ms);
 }
 
-void dally(const uint32_t ticks)
-{
-   const uint32_t end = millis() + ticks;
-   
-   while (millis() < end)
-      ;
-}
-
 
 /* setRGBLed --- control RGB LED connected to PORT B */
 
-void setRGBLed(const int state)
+void setRGBLed(const int state, const uint8_t fade)
 {
    switch (state) {
-   case 0:                    // Red
+   case 0:                    // Red fading up, blue on
+      TCA0.SINGLE.CMP0 = fade;
+      TCA0.SINGLE.CMP1 = 0;
+      TCA0.SINGLE.CMP2 = 255;
       PORTB.OUTSET = LED_R;
       PORTB.OUTCLR = LED_G;
       PORTB.OUTCLR = LED_B;
       break;
-   case 1:                    // Yellow
+   case 1:                    // Red on, blue fading down
+      TCA0.SINGLE.CMP0 = 255;
+      TCA0.SINGLE.CMP1 = 0;
+      TCA0.SINGLE.CMP2 = 255 - fade;
       PORTB.OUTSET = LED_R;
       PORTB.OUTSET = LED_G;
       PORTB.OUTCLR = LED_B;
       break;
-   case 2:                    // Green
+   case 2:                    // Red on, green fading up
+      TCA0.SINGLE.CMP0 = 255;
+      TCA0.SINGLE.CMP1 = fade;
+      TCA0.SINGLE.CMP2 = 0;
       PORTB.OUTCLR = LED_R;
       PORTB.OUTSET = LED_G;
       PORTB.OUTCLR = LED_B;
       break;
-   case 3:                    // Cyan
+   case 3:                    // Red fading down, green on
+      TCA0.SINGLE.CMP0 = 255 - fade;
+      TCA0.SINGLE.CMP1 = 255;
+      TCA0.SINGLE.CMP2 = 0;
       PORTB.OUTCLR = LED_R;
       PORTB.OUTSET = LED_G;
       PORTB.OUTSET = LED_B;
       break;
-   case 4:                    // Blue
+   case 4:                    // Green on, blue fading up
+      TCA0.SINGLE.CMP0 = 0;
+      TCA0.SINGLE.CMP1 = 255;
+      TCA0.SINGLE.CMP2 = fade;
       PORTB.OUTCLR = LED_R;
       PORTB.OUTCLR = LED_G;
       PORTB.OUTSET = LED_B;
       break;
-   case 5:                    // Magenta
+   case 5:                    // Green fading down, blue on
+      TCA0.SINGLE.CMP0 = 0;
+      TCA0.SINGLE.CMP1 = 255 - fade;
+      TCA0.SINGLE.CMP2 = 255;
       PORTB.OUTSET = LED_R;
       PORTB.OUTCLR = LED_G;
       PORTB.OUTSET = LED_B;
@@ -99,6 +111,8 @@ void setRGBLed(const int state)
 int main(void)
 {
    int ledState = 0;
+   uint8_t fade = 0;
+   uint32_t end;
    
    _PROTECTED_WRITE(CLKCTRL.MCLKCTRLA, CLKCTRL_CLKSEL_OSC20M_gc); // Select 20MHz RC oscillator
    
@@ -113,29 +127,50 @@ int main(void)
    PORTB.OUT = LED_R;
    PORTC.OUT = 0xFF;
 
-   TCA0.SINGLE.PER = 1250 - 1;
-   TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV16_gc;
-   TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_NORMAL_gc;
+   // Set up TCA0 for three PWM outputs
+   TCA0.SINGLE.PER = 255;
+   TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc;
+   TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_CMP1EN_bm | TCA_SINGLE_CMP2EN_bm;
    TCA0.SINGLE.CTRLC = 0;
    TCA0.SINGLE.CTRLD = 0;
-   TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
+   TCA0.SINGLE.CMP0 = 0;   // Red PWM
+   TCA0.SINGLE.CMP1 = 0;   // Green PWM
+   TCA0.SINGLE.CMP2 = 16;  // Blue PWM
    TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+   
+   // Set up TCB0 for regular 1ms interrupt
+   TCB0.CTRLA = TCB_CLKSEL_CLKDIV2_gc;
+   TCB0.CTRLB = TCB_CNTMODE_INT_gc;
+   TCB0.CCMP = 9999;             // 10000 counts gives 1ms
+   TCB0.CNT = 0;
+   TCB0.INTCTRL = TCB_CAPT_bm;   // Enable interrupts
+   TCB0.CTRLA |= TCB_ENABLE_bm;  // Enable timer
    
    sei();   // Enable interrupts
    
+   end = millis() + 500UL;
+   
    while (1) {
-      PORTA.OUTSET = LED;        // LED on PA1 on
-      setRGBLed(ledState++);
-      if (ledState > 5)
-         ledState = 0;
-      
-      dally(500UL);
+      if (Tick) {
+         if (fade == 255) {
+            fade = 0;
 
-      PORTA.OUTCLR = LED;        // LED on PA1 off
-      setRGBLed(ledState++);
-      if (ledState > 5)
-         ledState = 0;
-      
-      dally(500UL);
+            if (ledState == 5)
+               ledState = 0;
+            else
+               ledState++;
+         }
+         else
+            fade++;
+            
+         setRGBLed(ledState, fade);
+
+         if (millis() > end) {
+            PORTA.OUTTGL = LED;        // LED on PA1 toggle
+            end = millis() + 500UL;
+         }
+         
+         Tick = 0;
+      }
    }
 }

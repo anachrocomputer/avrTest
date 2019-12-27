fdd008000dd97aa5fa7c7561fd4b91ff31d07614
John Honniball <coredump@virginmedia.com>
2019-12-27 00:45:36 +0000

    Add circular buffer and interrupt logic to UART0 on the ATmega1284P. Code cribbed from ATtiny1616 implementation and modified for old-style USART registers and ISR names. Also add rudimentary command parser with just a single command so far, 'r'.

--- t1284p.c
+++ t1284p.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #define LED PB0
@@ -15,7 +16,80 @@
 #define BAUDRATE (9600)
 #define BAUD_SETTING ((F_CPU / (BAUDRATE * 16UL)) - 1)
 
+#define UART_RX_BUFFER_SIZE  (128)
+#define UART_RX_BUFFER_MASK (UART_RX_BUFFER_SIZE - 1)
+#if (UART_RX_BUFFER_SIZE & UART_RX_BUFFER_MASK) != 0
+#error UART_RX_BUFFER_SIZE must be a power of two and <= 256
+#endif
+
+#define UART_TX_BUFFER_SIZE  (128)
+#define UART_TX_BUFFER_MASK (UART_TX_BUFFER_SIZE - 1)
+#if (UART_TX_BUFFER_SIZE & UART_TX_BUFFER_MASK) != 0
+#error UART_TX_BUFFER_SIZE must be a power of two and <= 256
+#endif
+
+struct UART_RX_BUFFER
+{
+    volatile uint8_t head;
+    volatile uint8_t tail;
+    uint8_t buf[UART_RX_BUFFER_SIZE];
+};
+
+struct UART_TX_BUFFER
+{
+    volatile uint8_t head;
+    volatile uint8_t tail;
+    uint8_t buf[UART_TX_BUFFER_SIZE];
+};
+
+struct UART_BUFFER
+{
+    struct UART_TX_BUFFER tx;
+    struct UART_RX_BUFFER rx;
+};
+
+// UART buffers
+struct UART_BUFFER U0Buf;
+
 uint8_t SavedMCUSR = 0;
+
+
+/* USART0_RX_vect --- ISR for USART0 Receive Complete, used for Rx */
+
+ISR(USART0_RX_vect)
+{
+   const uint8_t tmphead = (U0Buf.rx.head + 1) & UART_RX_BUFFER_MASK;
+   const uint8_t ch = UDR0;  // Read received byte from UART
+   
+   if (tmphead == U0Buf.rx.tail)   // Is receive buffer full?
+   {
+       // Buffer is full; discard new byte
+   }
+   else
+   {
+      U0Buf.rx.head = tmphead;
+      U0Buf.rx.buf[tmphead] = ch;   // Store byte in buffer
+   }
+}
+
+
+/* USART0_UDRE_vect --- ISR for USART0 Data Register Empty, used for Tx */
+
+ISR(USART0_UDRE_vect)
+{
+   if (U0Buf.tx.head != U0Buf.tx.tail) // Is there anything to send?
+   {
+      const uint8_t tmptail = (U0Buf.tx.tail + 1) & UART_TX_BUFFER_MASK;
+      
+      U0Buf.tx.tail = tmptail;
+
+      UDR0 = U0Buf.tx.buf[tmptail];    // Transmit one byte
+   }
+   else
+   {
+      UCSR0B &= ~(1 << UDRIE0);  // Nothing left to send; disable Tx interrupt
+   }
+}
 
 void t1ou0(const int ch)
 {
@@ -34,6 +108,37 @@ void t1ou1(const int ch)
 }
 
 
+/* UART0RxByte --- read one character from the UART via the circular buffer */
+
+uint8_t UART0RxByte(void)
+{
+   const uint8_t tmptail = (U0Buf.rx.tail + 1) & UART_RX_BUFFER_MASK;
+   
+   while (U0Buf.rx.head == U0Buf.rx.tail)  // Wait, if buffer is empty
+       ;
+   
+   U0Buf.rx.tail = tmptail;
+   
+   return (U0Buf.rx.buf[tmptail]);
+}
+
+
+/* UART0TxByte --- send one character to the UART via the circular buffer */
+
+void UART0TxByte(const uint8_t data)
+{
+   const uint8_t tmphead = (U0Buf.tx.head + 1) & UART_TX_BUFFER_MASK;
+   
+   while (tmphead == U0Buf.tx.tail)   // Wait, if buffer is full
+       ;
+
+   U0Buf.tx.buf[tmphead] = data;
+   U0Buf.tx.head = tmphead;
+
+   UCSR0B |= (1 << UDRIE0);   // Enable UART0 Tx interrupt
+}
+
+
 /* USART0_printChar --- helper function to make 'stdio' functions work */
 
 static int USART0_printChar(const char c, FILE *stream)
@@ -47,6 +152,14 @@ static int USART0_printChar(const char c, FILE *stream)
 }
 
 static FILE USART_stream = FDEV_SETUP_STREAM(USART0_printChar, NULL, _FDEV_SETUP_WRITE);
+
+
+/* UART0RxAvailable --- return true if a byte is available in the UART circular buffer */
+
+int UART0RxAvailable(void)
+{
+   return (U0Buf.rx.head != U0Buf.rx.tail);
+}
 
 
 /* printResetReason --- print the cause of the chip's reset */
@@ -72,7 +185,7 @@ int main(void)
    UBRR0H = (uint8_t)(BAUD_SETTING >> 8); 
    UBRR0L = (uint8_t)(BAUD_SETTING);
    // Enable receive and transmit
-   UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+   UCSR0B = (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0);
    // Set frame format
    UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);  // Async 8N1
 
@@ -99,6 +212,8 @@ int main(void)
    OCR1B = 0x80;
 #endif
 
+   sei();   // Enable interrupts
+   
    t1ou1('\r');
    t1ou1('\n');
 
@@ -126,8 +241,7 @@ int main(void)
       // Switch LED on
       PORTB |= 1 << LED;
 
-      t1ou0('U');
-      t1ou0('U');
+      printf("LED ON\n");
 
       t1ou1('U');
       t1ou1('U');
@@ -139,8 +253,7 @@ int main(void)
       // Switch LED off
       PORTB &= ~(1 << LED);
 
-      t1ou0('A');
-      t1ou0('B');
+      printf("LED OFF\n");
       
       t1ou1('C');
       t1ou1('D');
@@ -148,5 +261,17 @@ int main(void)
       _delay_ms(500);
       
       i = (i + 1) & 0x07;
+      
+      if (UART0RxAvailable()) {
+         const char ch = UART0RxByte();
+         
+         printf("UART0: %02x\n", ch);
+         switch (ch) {
+         case 'r':
+         case 'R':
+            printResetReason();
+            break;
+         }
+      }
    }
 }
